4626e17ad00a1d283fbb05e8f33eeddfef409ee5
lorenzomaridev <lorenzomari.dev@gmail.com>
2023-04-28 01:15:09 +0200

    initial BIG TASK 1/2 display users

--- resources/js/components/pages/AvatarPage.vue
+++ resources/js/components/pages/AvatarPage.vue
@@ -6,6 +6,9 @@
 
             <div class="allineatore d-flex container-fluid flex-column justify-content-center px-4 mt-2 align-items-center">
 
+
+
+                <!-- Like in the rank page display logos of selected player (selected playaer = AVATAR CARD -->
                 <!------------------------ AVATAR CARD ---------------------->
                 <div style='margin-bottom:50px; margin-top:-1px; background-color:black; border:3px solid white;' class="container-sm avatar d-flex flex-column  align-items-center px-3">
                     <img style='width:50px; position: absolute; top:15px; left:15px;' src='images/extra_objects/ff.png' alt="">
@@ -19,15 +22,36 @@
                         </p>
                     </div>
                     <div class='d-flex justify-content-center flex-row align-items-center' style='margin-bottom:0;' >
-                        <img style='width:50px; ' src='images/board/animals/icon-60.png' alt="">
-                        <img style='width:29px; margin:0px 5px; filter: invert(0);' src='images/extra_objects/8.png' alt="">
-                        <img style='width:29px; margin:0px 5px; filter: invert(0);' src='images/extra_objects/mario.png' alt="">
-                        <img style='width:28px; margin:0px 5px; filter: invert(0);' src='images/extra_objects/palm.png' alt="">
-                        <img style='width:29px; margin:0px 5px; filter: invert(0);' src='images/extra_objects/ghost.png' alt="">
+                        <img style=' filter: invert(1); width:50px; ' src='images/board/animals/icon-59.png' alt=""> <!-- nft_9_cat  -->
+                        <img style=' filter: invert(1); width:50px; ' src='images/board/animals/icon-36.png' alt=""> <!-- nft_10_monkey -->
+                        <img style=' filter: invert(1); width:50px; ' src='images/board/animals/icon-39.png' alt=""> <!-- nft_22_leo_3 -->
+                        <img style=' filter: invert(1); width:50px; ' src='images/board/animals/icon-38.png' alt="">   <!-- nft_22_leo_2 -->
+                        <img style=' filter: invert(1); width:50px; ' src='images/board/animals/icon-37.png' alt=""> <!-- nft_22_leo_1 -->
+                        <img style='width:29px; margin:0px 5px; filter: invert(0);' src='images/extra_objects/8.png' alt=""> <!-- nft_13_doodle -->
+                        <img style=' filter: invert(0); width:29px; margin:0px 5px; ' src='images/extra_objects/mario.png' alt=""> <!-- nft_14_sandbox -->
+                        <img style=' filter: invert(0); width:28px; margin:0px 5px; ' src='images/extra_objects/palm.png' alt=""> <!-- nft_20_land -->
+                        <img style=' filter: invert(0); width:20px; margin:0px 5px; ' src='images/extra_objects/ghost.png' alt=""> <!-- nft_15_totem -->
+                        <i style=' filter: invert(1); font-size:20px; color:black; margin:0px 5px;'  class="fa-solid fa-hand-fist"></i> <!-- nft_11_punks -->
+                        <i style=' filter: invert(1); font-size:20px; color:black; margin:0px 5px;' class="fa-solid fa-egg"></i> <!-- nft_12_monster -->
+                        <img style=' filter: invert(0); width:25px; margin-left:5px; height:13px;' src='images/extra_objects/nike.png' alt=""> <!-- nft_16_nike  -->
+                        <img style=' filter: invert(1); width:25px; margin:0px 5px; ' src='images/extra_objects/adidas.png' alt=""> <!-- nft_17_adidas  -->
+                        <img style=' filter: invert(1); width:30px; margin:0px 5px; ' src='images/extra_objects/coco.png' alt=""> <!-- nft_19_lacoste -->
+                        <img style=' filter: invert(0); width:20px; margin:0px 5px; ' src='images/extra_objects/wb.png' alt=""> <!--  -->
+                        <img style=' filter: invert(1); width:20px; margin:0px 5px; filter: ' src='images/extra_objects/pepsi.png' alt=""> <!-- nft_18_pepsi -->
+                        <img style=' filter: invert(1); width:22px; margin:0px 5px; filter: ' src='images/extra_objects/bot.png' alt=""> <!-- nft_8_bot -->
+                        <img style=' filter: invert(1); width:23px; margin:0px 5px; filter: ' src='images/extra_objects/raygun.png' alt=""> <!-- nft_21_raygun -->
+                        <i style=' filter: invert(1); font-size:20px; color:black; margin:0px 5px;'  class="fa-solid fa-battery-full"></i> <!-- nft_3_battery -->
                     </div>
                     <p style='font-size:33px; color:white;'>18 <i style='color:#EE5E81' class="fa-solid fa-bolt"></i></p>
                 </div>
                 <!------------------------------------------------------------>
+
+
+
+
+
+
+
 
                 <!------------------------ HISTORY EXAMPLE ---------------------->
                 <div>
--- resources/js/components/pages/RankPage.vue
+++ resources/js/components/pages/RankPage.vue
@@ -52,45 +52,45 @@
                 </p>
             </button>
 
+
+
+            <!-- 1 May BIG TASK : 
+            - display a record class div model for any user in database ordered by power 
+            - if user field written in comment is true (or more then 0) show image 
+             -->
             <!---------------------------- User record examaples ---------------------------->
             <div style="background-color:#EDEB52;"  class="record">
                 <p  style='color:black; font-size:x-smal; padding: 10px; margin: 0; text-overflow: ellipsis; white-space: nowrap;  overflow:hidden;'>1°
                     <img style='width:20px; margin-left:5px; margin-right:5px;  margin-bottom:3px;'   src='images/extra_objects/iconaplayB.png'>
                     <!-- <img style='width:20px; margin-left:5px; margin-right:5px;  margin-bottom:3px; filter: invert(1);'   src='images/extra_objects/punk.png'> --> 
 
-                    0xF83611F45e11b590eBB9FdABa9ee12e7Dc9E9393
+                    0xF83611F45e11b590eBB9FdABa9ee12e7Dc9E9393 <!-- wallet_address -->
                 </p>
                 <div class='d-flex align-items-center flex-row flex-nowrap'>
-                    <img style='width:35px; ' src='images/board/animals/icon-39.png' alt="">
-                    <img style='width:35px; ' src='images/board/animals/icon-38.png' alt="">
-                    <img style='width:35px; ' src='images/board/animals/icon-37.png' alt="">
-                    <img style='width:35px; ' src='images/board/animals/icon-36.png' alt="">
-                    <img style='width:35px; ' src='images/board/animals/icon-59.png' alt="">
-                    <img style='width:20px; margin:0px 5px; filter: invert(1);' src='images/extra_objects/8.png' alt="">
-                    <img style='width:20px; margin:0px 5px; filter: invert(1);' src='images/extra_objects/mario.png' alt="">
-                    <img style='width:18px; margin:0px 5px; filter: invert(1);' src='images/extra_objects/palm.png' alt="">
-                    <img style='width:20px; margin:0px 5px; filter: invert(1);' src='images/extra_objects/ghost.png' alt="">
-                    <i style='font-size:20px; color:black; margin:0px 5px;'  class="fa-solid fa-hand-fist"></i>
-                    <i style='font-size:20px; color:black; margin:0px 5px;' class="fa-solid fa-egg"></i>
-                    <img style='width:25px; margin-left:5px; height:13px; filter: invert(1);' src='images/extra_objects/nike.png' alt="">
-                    <img style='width:25px; margin:0px 5px; ' src='images/extra_objects/adidas.png' alt="">
-                    <img style='width:30px; margin:0px 5px; ' src='images/extra_objects/coco.png' alt="">
-                    <img style='width:20px; margin:0px 5px; filter: invert(1);' src='images/extra_objects/wb.png' alt="">
-                    <img style='width:20px; margin:0px 5px; filter: invert(1);' src='images/extra_objects/pepsi.png' alt="">
-                    <img style='width:22px; margin:0px 5px; filter: invert(1);' src='images/extra_objects/bot.png' alt="">
-                    <img style='width:23px; margin:0px 5px; filter: invert(1);' src='images/extra_objects/raygun.png' alt="">
-                    <i style='font-size:20px; color:black; margin:0px 5px;'  class="fa-solid fa-battery-full"></i>
+                    <img style='width:35px; ' src='images/board/animals/icon-59.png' alt=""> <!-- nft_9_cat  -->
+                    <img style='width:35px; ' src='images/board/animals/icon-36.png' alt=""> <!-- nft_10_monkey -->
+                    <img style='width:35px; ' src='images/board/animals/icon-39.png' alt=""> <!-- nft_22_leo_3 -->
+                    <img style='width:35px; ' src='images/board/animals/icon-38.png' alt="">   <!-- nft_22_leo_2 -->
+                    <img style='width:35px; ' src='images/board/animals/icon-37.png' alt=""> <!-- nft_22_leo_1 -->
+                    <img style='width:20px; margin:0px 5px; filter: invert(1);' src='images/extra_objects/8.png' alt=""> <!-- nft_13_doodle -->
+                    <img style='width:20px; margin:0px 5px; filter: invert(1);' src='images/extra_objects/mario.png' alt=""> <!-- nft_14_sandbox -->
+                    <img style='width:18px; margin:0px 5px; filter: invert(1);' src='images/extra_objects/palm.png' alt=""> <!-- nft_20_land -->
+                    <img style='width:20px; margin:0px 5px; filter: invert(1);' src='images/extra_objects/ghost.png' alt=""> <!-- nft_15_totem -->
+                    <i style='font-size:20px; color:black; margin:0px 5px;'  class="fa-solid fa-hand-fist"></i> <!-- nft_11_punks -->
+                    <i style='font-size:20px; color:black; margin:0px 5px;' class="fa-solid fa-egg"></i> <!-- nft_12_monster -->
+                    <img style='width:25px; margin-left:5px; height:13px; filter: invert(1);' src='images/extra_objects/nike.png' alt=""> <!-- nft_16_nike  -->
+                    <img style='width:25px; margin:0px 5px; ' src='images/extra_objects/adidas.png' alt=""> <!-- nft_17_adidas  -->
+                    <img style='width:30px; margin:0px 5px; ' src='images/extra_objects/coco.png' alt=""> <!-- nft_19_lacoste -->
+                    <img style='width:20px; margin:0px 5px; filter: invert(1);' src='images/extra_objects/wb.png' alt=""> <!--  -->
+                    <img style='width:20px; margin:0px 5px; filter: invert(1);' src='images/extra_objects/pepsi.png' alt=""> <!-- nft_18_pepsi -->
+                    <img style='width:22px; margin:0px 5px; filter: invert(1);' src='images/extra_objects/bot.png' alt=""> <!-- nft_8_bot -->
+                    <img style='width:23px; margin:0px 5px; filter: invert(1);' src='images/extra_objects/raygun.png' alt=""> <!-- nft_21_raygun -->
+                    <i style='font-size:20px; color:black; margin:0px 5px;'  class="fa-solid fa-battery-full"></i> <!-- nft_3_battery -->
                 </div>
-                <span class='align-items-center' style='color:black; margin-right: 10px;     white-space: nowrap;     background-color: ;  padding:0 10px;  border-radius: 20px; display:flex;'> 100  <i class="fa-solid fa-bolt ml-1"></i></span>
+                <span class='align-items-center' style='color:black; margin-right: 10px;     white-space: nowrap;     background-color: ;  padding:0 10px;  border-radius: 20px; display:flex;'> 100 <!-- power --> <i class="fa-solid fa-bolt ml-1"></i></span>
             </div>
             <!------------------------------------------------------------------------------>
-            <div style="background-color:#EE5E81;"  class="record ">
-                <p  style='color:black; font-size:x-smal; padding: 10px; margin: 0; text-overflow: ellipsis; white-space: nowrap;  overflow:hidden;'>2°
-                    <img style='width:20px; margin-left:5px; margin-right:5px;  margin-bottom:3px;' src='images/extra_objects/iconaplayB.png'> 0xF83611F45e11b590eBB9FdABa9ee12e7Dc9E9393
-                </p>
-                <span class='align-items-center' style='color:black; margin-right: 10px; white-space: nowrap; padding:0 10px; border-radius: 20px; display:flex;'> 6  <i class="fa-solid fa-bolt ml-1"></i></span>
-            </div>
-            <!------------------------------------------------------------------------------>
+
 
 
             <!-- Looping through players --------------------------------------------------->
@@ -107,6 +107,35 @@
                     <span class='align-items-center' style='color:black; margin-right: 10px;     white-space: nowrap;     background-color: ;  padding:0 10px;  border-radius: 20px; display:flex;'> 100  <i class="fa-solid fa-bolt ml-1"></i></span>
                 </div> -->
             <!------------------------------------------------------------------------------>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
             <!-------------------------------- RANK LIST EXAMPLE ---------------------------------------------- -->
