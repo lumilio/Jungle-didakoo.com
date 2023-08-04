19c72a3d99fcbce17e6db623c300c3aa0e5f5f69
lorenzomaridev <lorenzomari.dev@gmail.com>
2023-08-04 17:47:25 +0200

    Update info page

--- resources/js/components/pages/InfoPage.vue
+++ resources/js/components/pages/InfoPage.vue
@@ -17,24 +17,34 @@
             <!---------------------- BODY ------------------->
             <div class="allineatore d-flex justify-content-top ">
                 <div style="margin-top:30px;" class="container-sm pepar  ">
-                    <p style='font-size:30px;'>v 1.0.0
+                    <p style='font-size:30px;'>v 1.0.0 <a style="color:white; font-size:20px;" href="https://opensea.io/assets/ethereum/0xd07dc4262bcdbf85190c01c996b4c06a461d2430/628469"> 斗兽棋 </a>
                         <!-- <img style="width:40px; padding-bottom:20px;  margin: 0 5px;"   src='images/extra_objects/cubo2.png' alt=""><img style="width:40px; padding-bottom:20px;"   src='images/extra_objects/sunflower_1f33b.png' alt=""> -->
                     </p>
-                    <p>Ethereum game platform.</p>
-                    <p>Users Online 20/30 </p>
-                    <p>Actual power required to play:  0 z </p>
+                    <!-- <br width="100%" size="20" color="white"> -->
+                    <!-- <hr width="100%" size="20" color="white"> -->
+                    <p>Users Online : 20/30 </p>
+                    <p>Actual power required to play : 0<i class="fa-solid fa-bolt ml-1"></i> </p>
+                    <hr width="100%" size="20" color="white">
+                    <br width="100%" size="20" color="white">
                     <!-- <p>At the cout down end an immage of the entire rank will be mint. </p> -->
-                    <span style='color:white;'>Install an ethereum wallet to play and save your data ,    </span>
-                    <p> Options : <a style="color:#D1230F;" href="https://metamask.io/"> Metamask</a> , <a style="color:#D1230F;" href="https://metamask.io/">Coin Base</a></p>
+                    <span style='color:white;'>Install an ethereum wallet to play and save your data ,  Options :    </span>
+                    <span>> <a style="color:#D1230F;" href="https://metamask.io/"> Metamask</a> <</span>
+                    <span>> <a style="color:#D1230F;" href="https://metamask.io/">Coin Base</a> <</span>
+                    <br width="100%" size="20" color="white">
+                    <br width="100%" size="20" color="white">
+                    <br width="100%" size="20" color="white">
                     <!-- <p><a style="color:#D1230F;" href="https://metamask.io/">Coin Base</a></p> -->
                     <!-- <span style='color:white;'>To play you need an   <a style="color:#D1230F;" href="https://metamask.io/">Ethereum Wallet</a>  with </span> -->
                     <p>Buy Power Up Nft here :</p>
-                    <p>
-                        <a style="color:#FAEA09;" href="https://opensea.io/assets/ethereum/0xd07dc4262bcdbf85190c01c996b4c06a461d2430/628469">sun flower </a> 
-                        <!-- <a style="color:#FAEA09;" href="https://rarible.com/token/0xbdc8bd87cfd22ef5b90a93a77eebe5bd0628f841:2352475547185321859375352175754401687841756219705495990747967153936990732289?tab=overview">sun flower v.2 </a> -->
-                        token.
+
+                    <p style='font-size:20px;' >
+                        >>> <a style="color:#FAEA09;" href="https://opensea.io/assets/ethereum/0xd07dc4262bcdbf85190c01c996b4c06a461d2430/628469">sun flower </a> <<< 
+                        &nbsp  &nbsp 🌻  = 50<i class="fa-solid fa-bolt ml-1 "></i>
                     </p>
-                    <p style='font-size:20px;' >🌻  = 100 Z <!-- <img style='width:30px;' src='images/extra_objects/ff.png' alt=""> --></p>
+
+
+                    <hr width="100%" size="20" color="white">
+                    <br width="100%" size="20" color="white">
 
 
 
@@ -62,7 +72,8 @@
                     <p> > <a style="color:#556FEE;" href="https://opensea.io/collection/cryptopunks">The sandbox  </a> </p>
                     <p> > <a style="color:#556FEE;" href="https://opensea.io/collection/cryptopunks">Doodles  </a> </p>
                     <p> > <a style="color:#556FEE;" href="https://opensea.io/collection/cryptopunks">Hash Masks  </a> </p>
-                    <p>&#8203;</p>
+                    <hr width="100%" size="20" color="white">
+                    <br width="100%" size="20" color="white">
                     <!-- <p>&#8203;</p> -->
                     <!-- <p>Click the purple button to create a link for player 2</p> -->
                 <!-- <p>  1 2 3 4 5 6 7 8 1</p> -->
