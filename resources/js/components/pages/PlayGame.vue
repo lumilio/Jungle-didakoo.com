977590547539b358e9c1872aced614127cf207d7
ValterSahakyan <valtersahakyan92@gmail.com>
2023-09-26 17:03:10 +0400

    fix loading in PlayGame.

--- resources/js/components/pages/PlayGame.vue
+++ resources/js/components/pages/PlayGame.vue
@@ -286,6 +286,7 @@ export default {
         display: flex;
         height: 100vh;
         align-items: center;
+        justify-content: center;
     }
     .board-player .record {
         width: 100%;
