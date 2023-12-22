2b88e9ac851d3c1b304ce210f4863bd73e9bc0b4
ValterSahakyan <valtersahakyan92@gmail.com>
2023-12-22 19:48:32 +0400

    Fix lions nfts.

--- resources/js/constants/nftLinks.js
+++ resources/js/constants/nftLinks.js
@@ -1,9 +1,7 @@
-import MonkeyIconBlack from '../../images/board/animals/monkey_5.png';
-import MonkeyIconWhite from '../../images/board/animals/monkey_6.png';
-import LionIconBlack from '../../images/board/animals/lion_5.png';
-import LionIconWhite from '../../images/board/animals/lion_6.png';
-import CatIconBlack from '../../images/board/animals/cat_5.png';
-import CatIconWhite from '../../images/board/animals/cat_6.png';
+import MonkeyIconBlack from '../../../public/images/board/animals/icon-36.png';
+import MonkeyIconWhite from '../../../public/images/board/animals/icon-44.png';
+import CatIconBlack from '../../../public/images/board/animals/icon-59.png';
+import CatIconWhite from '../../../public/images/board/animals/icon-60.png';
 import NikeIcon from '../../images/extra_objects/nike.png';
 import AdidasIcon from '../../images/extra_objects/adidas.png';
 import WBIcon from '../../images/extra_objects/wb.png';
@@ -11,7 +9,12 @@ import PepsiIcon from '../../images/extra_objects/pepsi.png';
 import LacosteIcon from '../../images/extra_objects/coco.png';
 import LandIcon from '../../images/extra_objects/palm.png';
 import SandboxIcon from '../../images/extra_objects/mario.png';
-
+import tigerIconBlack from '../../../public/images/board/animals/icon-38.png'
+import tigerIconWhite from '../../../public/images/board/animals/icon-46.png'
+import leopardIconBlack from '../../../public/images/board/animals/icon-37.png'
+import leopardIconWhite from '../../../public/images/board/animals/icon-45.png'
+import LionIconBlack from '../../../public/images/board/animals/icon-39.png';
+import LionIconWhite from '../../../public/images/board/animals/icon-47.png';
 
 export default function colorIconNft(colorIcon){
     return {
@@ -36,8 +39,8 @@ export default function colorIconNft(colorIcon){
         nft_18_pepsi: PepsiIcon,
         nft_19_lacoste: LacosteIcon,
         nft_20_land: LandIcon,
-        nft_22_leo_1: (colorIcon === 3 || colorIcon === 5) ? LionIconWhite : LionIconBlack,
-        nft_23_leo_2: (colorIcon === 3 || colorIcon === 5) ? LionIconWhite : LionIconBlack,
+        nft_22_leo_1: (colorIcon === 3 || colorIcon === 5) ? leopardIconWhite : leopardIconBlack,
+        nft_23_leo_2: (colorIcon === 3 || colorIcon === 5) ? tigerIconWhite : tigerIconBlack,
         nft_24_leo_3: (colorIcon === 3 || colorIcon === 5) ? LionIconWhite : LionIconBlack,
         nft_25_wb: WBIcon,
     };
