f88072032721d8f3559f389fc211715b3fc67513
ValterSahakyan <valtersahakyan92@gmail.com>
2024-01-09 21:53:10 +0400

    fixed

--- resources/js/components/pages/PlayGame.vue
+++ resources/js/components/pages/PlayGame.vue
@@ -61,8 +61,9 @@
                                 class="d-flex align-items-center flex-row flex-nowrap"
                             >
                                 <template v-for="(item, key) in colorIconNft(colorUserTop)" v-if="(creatorUser ? opponent : creator)?.[key] > 0 && item">
-                                    <i v-if="key === 'nft_3_battery'" style="font-size: 24px"
+                                    <i v-if="key === 'nft_3_battery'" style="font-size: 24px; margin-right: 5px;"
                                        :style="{color: [3,5].includes(colorUserTop) ? 'white' : 'black'}" class="fa-solid fa-battery-full "></i>
+                                    <img v-else-if="key === 'nft_8_bot' || key === 'nft_21_raygun'" style="width:25px;" :src="item" alt=""/>
                                     <img style="width:30px;" :src="item" alt=""/>
                                 </template>
                             </div>
@@ -130,8 +131,9 @@
                                 class="d-flex align-items-center flex-row flex-nowrap"
                             >
                                 <template v-for="(item, key) in colorIconNft(colorUserBottom)" v-if="(creatorUser ? creator : opponent)?.[key] > 0 && item">
-                                    <i v-if="key === 'nft_3_battery'" style="font-size: 24px"
+                                    <i v-if="key === 'nft_3_battery'" style="font-size: 24px; margin-right:  5px;"
                                        :style="{color: [3,5].includes(colorUserBottom) ? 'white' : 'black'}" class="fa-solid fa-battery-full "></i>
+                                    <img v-else-if="key === 'nft_8_bot' || key === 'nft_21_raygun'" style="width:25px;" :src="item" alt=""/>
                                     <img v-else style="width:30px;" :src="item" alt=""/>
                                 </template>
                             </div>
